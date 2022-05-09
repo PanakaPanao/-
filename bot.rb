ab9853a682270c748a0b193700f6dcb3a901e444
PanakaPanao <105179882+PanakaPanao@users.noreply.github.com>
2022-05-09 09:27:17 +0900

    Update and rename README.md to bot.rb

---
+++ bot.rb
@@ -0,0 +1,10 @@
+require 'discordrb'
+bot = Discordrb::Commands::CommandBot.new token: 'OTczMDA2MTE0NjQ2MDExOTI0.YnhUjw.zv7klDPqieenzmfkpq7onmFSyGc',
+  client_id: '973006114646011924'
+
+# 何かメッセージが送信されたら実行
+bot.message do |event|
+  event.respond 'hogehuga!'
+end
+
+bot.run
