a9f674200940dd80660527b8e6afb24102d64856
PanakaPanao <105179882+PanakaPanao@users.noreply.github.com>
2022-05-09 09:32:41 +0900

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -1,5 +1,5 @@
 require 'discordrb'
-bot = Discordrb::Commands::CommandBot.new token: 'OTczMDA2MTE0NjQ2MDExOTI0.YnhUjw.DochmzCzjbt_R1ND054d9vzexS4',
+bot = Discordrb::Commands::CommandBot.new token: 'OTczMDA2MTE0NjQ2MDExOTI0.YnhUjw.v9PUcTAVnM_WqW-lHmE4at3STo0',
   client_id: '973006114646011924'
 
 # 何かメッセージが送信されたら実行
